fa93444316dca2e532d2ac9e05716d2c1bfb510f
Alfred <alfred_bit@sina.cn>
2017-11-14 08:46:51 +0800

    E:/Program Files (x86)/Git/Git/cluster/clusterTest.py and cluster.py edited to be executable

--- cluster/clusterTest.py
+++ cluster/clusterTest.py
@@ -12,11 +12,18 @@ clust = cluster.hcluster(data)
 
 cluster.drawdendrogram(clust, blognames, jpeg = 'blogclust.jpg')
 '''
-
+'''
 kclust = cluster.kcluster(data, k = 10)
 
 print([blognames[r] for r in kclust[2]])
-
+'''
+'''
+wants, people, data = cluster.readfile('zebo.txt')
+clust = cluster.hcluster(data, distance = cluster.tanimoto)
+cluster.drawdendrogram(clust, wants)
+'''
+coords = cluster.scaledown(data)
+cluster.draw2d(coords, blognames, jpeg = 'blogs2d.jpg')
 
 
 
