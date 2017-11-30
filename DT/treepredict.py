807b0aa44d1decce699923ce8f763af41533f03c
Alfred <alfred_bit@sina.cn>
2017-11-30 21:39:53 +0800

    E:/Program Files (x86)/Git/Git/DT/treepredict.py edited

--- DT/treepredict.py
+++ DT/treepredict.py
@@ -155,6 +155,24 @@ def drawnode(draw, tree, x, y):
 		txt = ' \n'.join(['%s:%d' %v for v in tree.results.items()])
 		draw.text((x-20, y), txt, (0, 0, 0))
 
+def classify(observation, tree):
+	if tree.results != None:
+		return tree.results
+	else:
+		v = observation[tree.col]
+		branch = None
+		if isinstance(v, int) or isinstance(v, float):
+			if v >= tree.value:
+				branch = tree.tb
+			else:
+				branch = tree.fb
+		else:
+			if v == tree.value:
+				branch = tree.tb
+			else:
+				branch = tree.fb
+			return classify(observation, branch)
+
 
 	
 
